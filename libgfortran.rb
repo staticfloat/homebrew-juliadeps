1cabe76ec53cebebdb4141ffcfd27316e363967a
Elliot Saba <staticfloat@gmail.com>
2013-10-21 20:46:36 -0700

    Do post_install to get libraries in the necessary place

--- libgfortran.rb
+++ libgfortran.rb
@@ -14,6 +14,16 @@ class Libgfortran < Formula
     sha1 '9577f77038188a4f4245dded282670e868994cef' => :snow_leopard
   end
 
+  def post_install
+    # This is here to symlink the libs to alternate locations where they could be installed
+    mkdir prefix+'gfortran/lib'
+    for lib in ['quadmath.0', 'gcc_s.1', 'gfortran.3']
+      quiet_system 'ln', '-fs', prefix+"gfortran/lib/lib#{lib}.dylib", lib+"lib#{lib}.dylib"
+      quiet_system 'ln', '-fs', prefix+"gfortran/lib/lib#{lib[0..-3]}.dylib", lib+"lib#{lib}.dylib"
+      quiet_system 'ln', '-fs', lib+"lib#{lib[0..-3]}.dylib", lib+"lib#{lib}.dylib"
+    end
+  end
+
   def install
     opoo "This formula is bottle-only!  If you actually want to install gfortran, use the gfortran formula included by brew!"
   end
