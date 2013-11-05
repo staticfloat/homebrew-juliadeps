381c9c6870b460a95d7b3847f813ee44181a6f71
Elliot Saba <staticfloat@gmail.com>
2013-11-04 23:59:54 -0800

    Bump freetype bottle

--- freetype.rb
+++ freetype.rb
@@ -5,6 +5,15 @@ class Freetype < Formula
   url 'http://downloads.sf.net/project/freetype/freetype2/2.5.0/freetype-2.5.0.1.tar.gz'
   sha1 '2d539b375688466a8e7dcc4260ab21003faab08c'
 
+  bottle do
+    root_url 'http://archive.org/download/julialang/bottles'
+    cellar :any
+    sha1 'dadd3adf072bb61d4aec0ee102fc8113177dcd9e' => :mountain_lion
+    sha1 'dadd3adf072bb61d4aec0ee102fc8113177dcd9e' => :lion
+    sha1 'dadd3adf072bb61d4aec0ee102fc8113177dcd9e' => :snow_leopard
+    sha1 'dadd3adf072bb61d4aec0ee102fc8113177dcd9e' => :mavericks
+  end
+
   option :universal
   depends_on 'staticfloat/juliadeps/libpng'
 
