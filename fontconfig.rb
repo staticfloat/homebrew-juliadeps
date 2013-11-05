e484dea7da6fa6ce4a074c2b7242f892e016a705
Elliot Saba <staticfloat@gmail.com>
2013-11-05 00:04:20 -0800

    Bump fontconfig bottle

--- fontconfig.rb
+++ fontconfig.rb
@@ -7,6 +7,15 @@ class Fontconfig < Formula
 
   option :universal
 
+  bottle do
+    root_url 'http://archive.org/download/julialang/bottles'
+    cellar :any
+    sha1 '5a12aa1eeecb2aa8584515a0680eea432d9f85e0' => :mountain_lion
+    sha1 '5a12aa1eeecb2aa8584515a0680eea432d9f85e0' => :lion
+    sha1 '5a12aa1eeecb2aa8584515a0680eea432d9f85e0' => :snow_leopard
+    sha1 '5a12aa1eeecb2aa8584515a0680eea432d9f85e0' => :mavericks
+  end
+
   depends_on 'staticfloat/juliadeps/freetype'
   depends_on 'staticfloat/juliadeps/pkg-config' => :build
 
