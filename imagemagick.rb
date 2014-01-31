4ad49645f0deac04aed6f273eb836446caeb7f0e
Elliot Saba <staticfloat@gmail.com>
2014-01-31 15:40:18 -0800

    Add Imagemagick, just for you, @timholy!

---
+++ imagemagick.rb
@@ -0,0 +1,116 @@
+require 'formula'
+
+class Imagemagick < Formula
+  homepage 'http://www.imagemagick.org'
+
+  # upstream's stable tarballs tend to disappear, so we provide our own mirror
+  # Tarball and checksum from: http://www.imagemagick.org/download
+  url 'http://downloads.sf.net/project/machomebrew/mirror/ImageMagick-6.8.7-7.tar.bz2'
+  sha256 '4d8b0889d78cca2f1501b5f66f61c5efcd2f585a03002f2a7b407c11808e5e28'
+
+  head 'https://www.imagemagick.org/subversion/ImageMagick/trunk',
+    :using => UnsafeSubversionDownloadStrategy
+
+  bottle do
+    root_url 'http://archive.org/download/julialang/bottles'
+    cellar :any
+    revision 1
+    sha1 '58022e696acc51c3bf87250d61287abe2cb399f4' => :mountain_lion
+    sha1 '58022e696acc51c3bf87250d61287abe2cb399f4' => :lion
+    sha1 '58022e696acc51c3bf87250d61287abe2cb399f4' => :snow_leopard
+    sha1 '58022e696acc51c3bf87250d61287abe2cb399f4' => :mavericks
+  end
+
+  option 'with-quantum-depth-8', 'Compile with a quantum depth of 8 bit'
+  option 'with-quantum-depth-16', 'Compile with a quantum depth of 16 bit'
+  option 'with-quantum-depth-32', 'Compile with a quantum depth of 32 bit'
+  option 'with-perl', 'enable build/install of PerlMagick'
+  option 'without-magick-plus-plus', 'disable build/install of Magick++'
+
+  depends_on :libtool => :run
+
+  depends_on 'pkg-config' => :build
+
+  depends_on 'jpeg' => :recommended
+  depends_on :libpng => :recommended
+  depends_on :freetype => :recommended
+
+  depends_on :x11 => :optional
+  depends_on :fontconfig => :optional
+  depends_on 'libtiff' => :optional
+  depends_on 'little-cms' => :optional
+  depends_on 'little-cms2' => :optional
+  depends_on 'jasper' => :optional
+  depends_on 'libwmf' => :optional
+  depends_on 'librsvg' => :optional
+  depends_on 'liblqr' => :optional
+  depends_on 'openexr' => :optional
+  depends_on 'ghostscript' => :optional
+  depends_on 'webp' => :optional
+
+  opoo '--with-ghostscript is not recommended' if build.with? 'ghostscript'
+
+  def pour_bottle?
+    # If libtool is keg-only it currently breaks the bottle.
+    # This is a temporary workaround until we have a better fix.
+    not Formula.factory('libtool').keg_only?
+  end
+
+  skip_clean :la
+
+  def install
+    args = [ "--disable-osx-universal-binary",
+             "--prefix=#{prefix}",
+             "--disable-dependency-tracking",
+             "--enable-shared",
+             "--disable-static",
+             "--without-pango",
+             "--with-modules",
+             "--disable-openmp"]
+
+    args << "--disable-opencl" if build.include? 'disable-opencl'
+    args << "--without-gslib" unless build.with? 'ghostscript'
+    args << "--without-perl" unless build.with? 'perl'
+    args << "--with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts" unless build.with? 'ghostscript'
+    args << "--without-magick-plus-plus" if build.without? 'magick-plus-plus'
+    args << "--enable-hdri=yes" if build.include? 'enable-hdri'
+
+    if build.with? 'quantum-depth-32'
+      quantum_depth = 32
+    elsif build.with? 'quantum-depth-16'
+      quantum_depth = 16
+    elsif build.with? 'quantum-depth-8'
+      quantum_depth = 8
+    end
+
+    args << "--with-quantum-depth=#{quantum_depth}" if quantum_depth
+    args << "--with-rsvg" if build.with? 'librsvg'
+    args << "--without-x" unless build.with? 'x11'
+    args << "--with-fontconfig=yes" if build.with? 'fontconfig'
+    args << "--with-freetype=yes" if build.with? 'freetype'
+    args << "--with-webp=yes" if build.include? 'webp'
+
+    # versioned stuff in main tree is pointless for us
+    inreplace 'configure', '${PACKAGE_NAME}-${PACKAGE_VERSION}', '${PACKAGE_NAME}'
+    system "./configure", *args
+    system "make install"
+  end
+
+  def caveats
+    s = <<-EOS.undent
+      For full Perl support you must install the Image::Magick module from the CPAN.
+        https://metacpan.org/module/Image::Magick
+
+      The version of the Perl module and ImageMagick itself need to be kept in sync.
+      If you upgrade one, you must upgrade the other.
+
+      For this version of ImageMagick you should install
+      version #{version} of the Image::Magick Perl module.
+    EOS
+    s if build.with? 'perl'
+  end
+
+  test do
+    system "#{bin}/identify", "/usr/share/doc/cups/images/cups.png"
+  end
+end
