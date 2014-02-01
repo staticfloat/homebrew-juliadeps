8056f15e4a2e3d6d32f0a38d8978b46e38e789ae
Elliot Saba <staticfloat@gmail.com>
2014-02-01 01:25:28 -0800

    Update harfbuzz

--- harfbuzz.rb
+++ harfbuzz.rb
@@ -2,23 +2,19 @@ require 'formula'
 
 class Harfbuzz < Formula
   homepage 'http://www.freedesktop.org/wiki/Software/HarfBuzz'
-  url 'http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.21.tar.bz2'
-  sha256 'a0d36f883d98f2375f9b2a03c8a6c361b161a76d4b58ea9d08be59e39e73dae8'
-
-  depends_on 'staticfloat/juliadeps/pkg-config' => :build
-  depends_on 'staticfloat/juliadeps/glib'
-  depends_on 'staticfloat/juliadeps/cairo'
-  depends_on 'freetype'
+  url 'http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.25.tar.bz2'
+  sha256 'dc6e5997a569526cd28147a80a0f65466e87ae617753b38704a60184bc6d6bee'
 
   bottle do
     root_url 'http://archive.org/download/julialang/bottles'
     cellar :any
-    revision 1
-    sha1 '51134b8442512cb1d2131010c26980b5f2804cce' => :snow_leopard_or_later
+    sha1 '6ec0cacf740b67e4d57f3e1fda7a227b4272521a' => :snow_leopard_or_later
   end
 
-  # Needs newer fontconfig than XQuartz provides for pango
-  depends_on 'staticfloat/juliadeps/fontconfig'
+  depends_on 'staticfloat/juliadeps/pkg-config' => :build
+  depends_on 'staticfloat/juliadeps/glib'
+  depends_on 'staticfloat/juliadeps/cairo'
+  depends_on :freetype
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
