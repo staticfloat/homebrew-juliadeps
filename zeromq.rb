5afd607b934014b561e7f45a49e67b2ca65d54c5
Elliot Saba <staticfloat@gmail.com>
2014-02-01 01:39:44 -0800

    Update zeromq

--- zeromq.rb
+++ zeromq.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Zeromq32 < Formula
+class Zeromq < Formula
   homepage 'http://www.zeromq.org/'
   url 'http://download.zeromq.org/zeromq-3.2.4.tar.gz'
   sha1 '08303259f08edd1faeac2e256f5be3899377135e'
