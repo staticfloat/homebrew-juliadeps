9e235b6035fb95a439dda5a874976753a35005b7
Elliot Saba <staticfloat@gmail.com>
2014-07-25 16:03:09 -0400

    Move over glpk bottle

--- glpk.rb
+++ glpk.rb
@@ -7,12 +7,12 @@ class Glpk < Formula
   sha1 '44b30b0de777a0a07e00615ac6791af180ff4d2c'
 
   bottle do
-    root_url 'http://archive.org/download/julialang/bottles'
+    root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha1 'c1cf5b3ceb8c9275df469aeefeeb09631f1affff' => :mavericks
-    sha1 'c1cf5b3ceb8c9275df469aeefeeb09631f1affff' => :mountain_lion
-    sha1 'c1cf5b3ceb8c9275df469aeefeeb09631f1affff' => :lion
-    sha1 'c1cf5b3ceb8c9275df469aeefeeb09631f1affff' => :snow_leopard
+    revision 3
+    sha1 '75c88f64f6e1553ed384464b002cc8a8690f275b' => :lion
+    sha1 'e61fe1e67b932d6b80d81d91dcf6b4beb215c68f' => :mavericks
+    sha1 '15148c18db8712983c2fb3733636b9e989f1ca3b' => :mountain_lion
   end
 
   def install
