a7a7615a9fa3854a66e42cfcdcf31d5f173c8c3d
Elliot Saba <staticfloat@gmail.com>
2014-07-25 16:15:43 -0400

    Prepare zeromq32 for bottling

--- zeromq32.rb
+++ zeromq32.rb
@@ -5,28 +5,20 @@ class Zeromq32 < Formula
   url 'http://download.zeromq.org/zeromq-3.2.4.tar.gz'
   sha1 '08303259f08edd1faeac2e256f5be3899377135e'
 
-  bottle do
-    root_url 'http://archive.org/download/julialang/bottles'
-    cellar :any
-    sha1 '0f7adbef8efc7ef91cbf96a21de86124d8e283c4' => :snow_leopard_or_later
-  end
-
   option :universal
   option 'with-pgm', 'Build with PGM extension'
 
   depends_on 'staticfloat/juliadeps/pkg-config' => :build
-  depends_on 'libpgm' if build.include? 'with-pgm'
+  depends_on 'libpgm'
 
   def install
     ENV.universal_binary if build.universal?
-
     args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
-    if build.include? 'with-pgm'
-      # Use HB libpgm-5.2 because their internal 5.1 is b0rked.
-      ENV['OpenPGM_CFLAGS'] = %x[pkg-config --cflags openpgm-5.2].chomp
-      ENV['OpenPGM_LIBS'] = %x[pkg-config --libs openpgm-5.2].chomp
-      args << "--with-system-pgm"
-    end
+    
+    # Use HB libpgm-5.2 because their internal 5.1 is b0rked.
+    ENV['OpenPGM_CFLAGS'] = %x[pkg-config --cflags openpgm-5.2].chomp
+    ENV['OpenPGM_LIBS'] = %x[pkg-config --libs openpgm-5.2].chomp
+    args << "--with-system-pgm"
 
     system "./autogen.sh" if build.head?
     system "./configure", *args
