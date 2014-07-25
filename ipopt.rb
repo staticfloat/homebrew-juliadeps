2d51e55fd4ed9b586fb7364bc9981a91b0fb91cd
Elliot Saba <staticfloat@gmail.com>
2014-07-25 16:57:32 -0400

    We need gfortran to build

--- ipopt.rb
+++ ipopt.rb
@@ -5,7 +5,7 @@ class Ipopt < Formula
   url 'http://www.coin-or.org/download/source/Ipopt/Ipopt-3.11.8.tgz'
   sha1 '530d718fb5a0c994c305deb3bcfdacc16cc0e2ef'
 
-  
+  depends_on :fortran => :build
   depends_on 'staticfloat/juliadeps/libgfortran'
 
   def install
