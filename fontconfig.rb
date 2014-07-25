fecfd86ece2500eea84307c3d89c6f3465ed4ef4
Elliot Saba <staticfloat@gmail.com>
2014-07-25 15:57:37 -0400

    Bump Fontconfig to 2.11.1

--- fontconfig.rb
+++ fontconfig.rb
@@ -5,6 +5,14 @@ class Fontconfig < Formula
   url 'http://fontconfig.org/release/fontconfig-2.11.1.tar.bz2'
   sha1 '08565feea5a4e6375f9d8a7435dac04e52620ff2'
 
+  bottle do
+    root_url 'https://juliabottles.s3.amazonaws.com'
+    cellar :any
+    sha1 '8350de16baf1ae81f315935ea2037206fbd09aed' => :lion
+    sha1 '19898a3e5251c1d2c60643ef5f26ba7752784afa' => :mavericks
+    sha1 'f24b070d413a0286933774c3c9ce585c68250b32' => :mountain_lion
+  end
+
   option :universal
 
   depends_on 'freetype'
