55c743371678861cdaa5f7c5a03d8a9c922f387d
Elliot Saba <staticfloat@gmail.com>
2014-07-25 16:20:41 -0400

    Prepare nlopt for bottling

--- nlopt.rb
+++ nlopt.rb
@@ -2,18 +2,8 @@ require 'formula'
 
 class Nlopt < Formula
   homepage 'https://projects.coin-or.org/Ipopt'
-  url 'http://ab-initio.mit.edu/nlopt/nlopt-2.4.tar.gz'
-  sha1 'e766f4c49fa5923fb45220f278c01c04c38fc369'
-
-  bottle do
-    root_url 'http://archive.org/download/julialang/bottles'
-    cellar :any
-    revision 1
-    sha1 '08a94653c64cccd146b95ad5d3f0a14b36a0642e' => :mountain_lion
-    sha1 '08a94653c64cccd146b95ad5d3f0a14b36a0642e' => :lion
-    sha1 '08a94653c64cccd146b95ad5d3f0a14b36a0642e' => :snow_leopard
-    sha1 '5abcbc704aa7fca3d8bc507382f03e08b1b33e4a' => :mavericks
-  end
+  url 'http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz'
+  sha1 '838c399d8fffd7aa56b20231e0d7bd3462ca0226'
 
   def install
     ENV.deparallelize  # make install fails in parallel.
