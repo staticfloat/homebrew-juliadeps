adc4fc5efbc6da9684e70cbac039884239c89e0f
Elliot Saba <staticfloat@gmail.com>
2014-07-25 16:54:26 -0400

    Bottle libgfortran

--- libgfortran.rb
+++ libgfortran.rb
@@ -6,6 +6,14 @@ class Libgfortran < Formula
   sha1 '09328c065c42051fab341e660837704a3b1f5d4a'
   version '4.9.1'
 
+  bottle do
+    root_url 'https://juliabottles.s3.amazonaws.com'
+    cellar :any
+    sha1 '0e91476d934cf816f7e9f4b2035167bc92631c7f' => :lion
+    sha1 '3d74269dba281abbc1209beeabd9169054c8e6f8' => :mavericks
+    sha1 '03d5dce3825381bc4f4c8e6543f1753c96f4a653' => :mountain_lion
+  end
+
   def post_install
     # This is here to symlink the libs to alternate locations where they could be installed
     # we still need to do this, since otherwise mach_o_files doesn't find us, and we don't
