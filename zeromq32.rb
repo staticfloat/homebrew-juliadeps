c8b6dba6e66e68b1c7badaed5edeafe276b30af9
Elliot Saba <staticfloat@gmail.com>
2014-07-25 16:19:17 -0400

    Move zeromq32 bottle over

--- zeromq32.rb
+++ zeromq32.rb
@@ -5,6 +5,14 @@ class Zeromq32 < Formula
   url 'http://download.zeromq.org/zeromq-3.2.4.tar.gz'
   sha1 '08303259f08edd1faeac2e256f5be3899377135e'
 
+  bottle do
+    root_url 'https://juliabottles.s3.amazonaws.com'
+    cellar :any
+    sha1 '108a85f8909ac01aec2a79f79d89bb22f647e0be' => :lion
+    sha1 '8a86c0e14ce29098171070b1b053390af1604fbf' => :mavericks
+    sha1 '36af54400e7e3334fae374fdc79d0eb66a985b10' => :mountain_lion
+  end
+
   option :universal
   option 'with-pgm', 'Build with PGM extension'
 
