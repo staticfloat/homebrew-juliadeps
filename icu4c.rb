fbe5a77a6fcb0d4cb7b4cedb3eca2edfaec04fbd
Elliot Saba <staticfloat@gmail.com>
2014-07-26 16:37:23 -0400

    Bottle icu4c

--- icu4c.rb
+++ icu4c.rb
@@ -7,7 +7,14 @@ class Icu4c < Formula
   sha1 '6de440b71668f1a65a9344cdaf7a437291416781'
   head 'http://source.icu-project.org/repos/icu/icu/trunk/', :using => :svn
 
-
+  bottle do
+    root_url 'https://juliabottles.s3.amazonaws.com'
+    cellar :any
+    revision 2
+    sha1 'fb7c4555a0ec63f12e213c36c9fa4291d8c7f9be' => :lion
+    sha1 '53f8352c6f246844b967a8a8408335f5eb9f9baf' => :mavericks
+    sha1 'ca9f14db47473b2963ac63eaca4349c50f89b42b' => :mountain_lion
+  end
 
   keg_only "Conflicts; see: https://github.com/Homebrew/homebrew/issues/issue/167"
 
