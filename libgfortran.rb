cc4290e7270960133a81ce07600a9d4843b25931
Elliot Saba <staticfloat@gmail.com>
2014-07-27 00:53:59 -0400

    Of course, we can't do symlinks yet

--- libgfortran.rb
+++ libgfortran.rb
@@ -6,7 +6,25 @@ class Libgfortran < Formula
   url 'https://github.com/staticfloat/homebrew-libgfortran-formula/archive/master.tar.gz'
   sha1 '09328c065c42051fab341e660837704a3b1f5d4a'
   version '4.9.1'
-  revision 2
+  revision 1
+
+  bottle do
+    root_url 'https://juliabottles.s3.amazonaws.com'
+    cellar :any
+    revision 1
+    sha1 '4cb42189f6599af1536b900a6903b7383dffa146' => :lion
+    sha1 '80670a57d293dab0086408bb26a4480f99423ffe' => :mavericks
+    sha1 'ae384727435ee149a008bbf4b446f40094767752' => :mountain_lion
+  end
+
+  def post_install
+    # Symlink the libs into /lib as well, for easy access
+    Find.find("#{lib}") do |path|
+      if path =~ /#{lib}\/gcc\/[^\/]+\/[\d.]+\/lib[^\/]+\.dylib/
+        system 'ln', '-fs', path, "#{lib}/#{File.basename(path)[0..-9]}.dylib"
+      end
+    end
+  end
 
   def install
     # To generate a libgfortran installation, steal libraries from gcc!
@@ -21,13 +39,8 @@ class Libgfortran < Formula
           dest = "#{lib}/#{dest.tap{|s| s.slice!("#{Formula['gcc'].lib}/")}}"
           mkdir_p File.dirname(dest)
           system 'cp', path, dest
-
-          if dest =~ /#{lib}\/gcc\/[^\/]+\/[\d.]+\/lib[^\/]+\.dylib/
-            system 'ln', '-s', dest, "#{lib}/#{File.basename(path)[0..-9]}.dylib"
-          end
         end
       end
     end
   end
 end
-
