46c58be7ebaa7caa803dcb0750fc175848a5d9e7
Elliot Saba <staticfloat@gmail.com>
2014-07-27 00:50:24 -0400

    Revision two already!

--- libgfortran.rb
+++ libgfortran.rb
@@ -6,25 +6,7 @@ class Libgfortran < Formula
   url 'https://github.com/staticfloat/homebrew-libgfortran-formula/archive/master.tar.gz'
   sha1 '09328c065c42051fab341e660837704a3b1f5d4a'
   version '4.9.1'
-  revision 1
-
-  bottle do
-    root_url 'https://juliabottles.s3.amazonaws.com'
-    cellar :any
-    revision 1
-    sha1 '4cb42189f6599af1536b900a6903b7383dffa146' => :lion
-    sha1 '80670a57d293dab0086408bb26a4480f99423ffe' => :mavericks
-    sha1 'ae384727435ee149a008bbf4b446f40094767752' => :mountain_lion
-  end
-
-  def post_install
-    # Symlink the libs into /lib as well, for easy access
-    Find.find("#{lib}") do |path|
-      if path =~ /#{lib}\/gcc\/[^\/]+\/[\d.]+\/lib[^\/]+\.dylib/
-        system 'ln', '-s', path, "#{lib}/#{File.basename(path)[0..-9]}.dylib"
-      end
-    end
-  end
+  revision 2
 
   def install
     # To generate a libgfortran installation, steal libraries from gcc!
@@ -39,6 +21,7 @@ class Libgfortran < Formula
           dest = "#{lib}/#{dest.tap{|s| s.slice!("#{Formula['gcc'].lib}/")}}"
           mkdir_p File.dirname(dest)
           system 'cp', path, dest
+          system 'ln', '-s', dest, "#{lib}/#{File.basename(path)[0..-9]}.dylib"
         end
       end
     end
