37666817c30d707e3254b87da42cf753ef840b01
Elliot Saba <staticfloat@gmail.com>
2014-07-29 11:54:54 -0400

    Prepare gtk+3 for bottling

---
+++ gtk+3.rb
@@ -0,0 +1,38 @@
+require 'formula'
+
+class Gtkx3 < Formula
+  homepage 'http://gtk.org/'
+  url 'http://ftp.gnome.org/pub/gnome/sources/gtk+/3.12/gtk+-3.12.2.tar.xz'
+  sha256 '61d74eea74231b1ea4b53084a9d6fc9917ab0e1d71b69d92cbf60a4b4fb385d0'
+
+  depends_on 'staticfloat/juliadeps/pkg-config' => :build
+  depends_on 'xz' => :build
+  depends_on 'staticfloat/juliadeps/glib'
+  depends_on 'jpeg'
+  depends_on 'libtiff'
+  depends_on 'staticfloat/juliadeps/gdk-pixbuf'
+  depends_on 'staticfloat/juliadeps/pango'
+  depends_on 'staticfloat/juliadeps/cairo'
+  depends_on 'staticfloat/juliadeps/atk'
+
+  def install
+    # gtk-update-icon-cache is used during installation, and
+    # we don't want to add a dependency on gtk+2 just for this.
+    inreplace %w[ gtk/makefile.msc.in
+                  demos/gtk-demo/Makefile.in
+                  demos/widget-factory/Makefile.in ],
+                  /gtk-update-icon-cache --(force|ignore-theme-index)/,
+                  "#{buildpath}/gtk/\\0"
+
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--disable-glibtest",
+                          "--enable-introspection=yes",
+                          "--enable-x11-backend",
+                          "--disable-schemas-compile"
+    system "make install"
+    # Prevent a conflict between this and Gtk+2
+    mv bin/'gtk-update-icon-cache', bin/'gtk3-update-icon-cache'
+  end
+end
