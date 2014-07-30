997d9e356f4273b45dd1bb278d4c9b47638e4fea
Elliot Saba <staticfloat@gmail.com>
2014-07-30 12:51:24 -0400

    Ecos bottled!

--- ecos.rb
+++ ecos.rb
@@ -7,6 +7,14 @@ class Ecos < Formula
   sha1 "a43e64a72d803c45d2e8f69ed2f0fbfcc368692f"
   version "1.0.4.1"
 
+  bottle do
+    root_url 'https://juliabottles.s3.amazonaws.com'
+    cellar :any
+    sha1 '3851a33870c7bec6660e2ded37ff3086ae97e70a' => :lion
+    sha1 'edaefb6684cd4fd68e895230c8f2886b349b8ac4' => :mavericks
+    sha1 '621902182c2a158cfcfa729e2ff8c02c84f8cf07' => :mountain_lion
+  end
+
   def install
     system "make", "shared"
     system "make", "test"
