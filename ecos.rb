d490002dfccab28f3bd2ee792e1a064efd713224
Elliot Saba <staticfloat@gmail.com>
2014-07-30 12:45:51 -0400

    Add ecos, https://github.com/JuliaOpt/ECOS.jl/issues/6

---
+++ ecos.rb
@@ -0,0 +1,31 @@
+require "formula"
+
+class Ecos < Formula
+  homepage "https://github.com/ifa-ethz/ecos"
+  url "https://github.com/ifa-ethz/ecos/archive/d206a556a83396756f3200964de162b4a7523c62.tar.gz"
+  head "https://github.com/ifa-ethz/ecos.git"
+  sha1 "a43e64a72d803c45d2e8f69ed2f0fbfcc368692f"
+  version "1.0.4.1"
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    system "make", "shared"
+    system "make", "test"
+
+    # Install the real goodies
+    lib.install "libecos.a", "libecos.dylib"
+
+    # Install header files to <prefix>/include/ecos
+    include.install "include"
+    mv "#{include}/include", "#{include}/ecos"
+
+    # Install ecostester so we can test installation afterward!
+    bin.install "ecostester"
+  end
+
+  test do
+    system "#{bin}/ecostester"
+  end
+end
