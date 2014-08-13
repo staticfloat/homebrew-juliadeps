4c5030e8426be3d83aa2daad2d5135d08484b972
Elliot Saba <staticfloat@gmail.com>
2014-08-13 00:28:51 -0400

    Add bottles for newly versioned glpk formulae

--- glpk452.rb
+++ glpk452.rb
@@ -6,6 +6,14 @@ class Glpk452 < Formula
   mirror 'http://ftp.gnu.org/gnu/glpk/glpk-4.52.tar.gz'
   sha1 '44b30b0de777a0a07e00615ac6791af180ff4d2c'
 
+  bottle do
+    root_url 'https://juliabottles.s3.amazonaws.com'
+    cellar :any
+    sha1 '1cfc6a3e39ab1c3861cd2d33c88b31dfecbada9a' => :lion
+    sha1 '4e5035b64862a8ecf47637de9e7e2c0135e8d6e4' => :mavericks
+    sha1 'd5bbd247ef1c24fe54d0de67bb70ef227d6a33c1' => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
--- glpk454.rb
+++ glpk454.rb
@@ -6,6 +6,14 @@ class Glpk454 < Formula
   mirror 'http://ftp.gnu.org/gnu/glpk/glpk-4.54.tar.gz'
   sha1 '69d9c5d83271d34de1a58d2de2f7ababab975492'
 
+  bottle do
+    root_url 'https://juliabottles.s3.amazonaws.com'
+    cellar :any
+    sha1 '866ad909f2081a2d4bb4949426e2016e7a282550' => :lion
+    sha1 '20ed969847046a4d7bbda59c2e02756d1c33975d' => :mavericks
+    sha1 'd1148650ad7c8c3067c2ddf9625039324bb3db02' => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
