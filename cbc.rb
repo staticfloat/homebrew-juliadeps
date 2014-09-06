1b1892f45c4113edc120669b8b8b818cabe93a47
Miles Lubin <miles.lubin@gmail.com>
2014-09-06 17:43:43 -0400

    Add formula for Cbc

---
+++ cbc.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Cbc < Formula
+  homepage 'http://www.coin-or.org/projects/Cbc.xml'
+  url 'http://www.coin-or.org/download/source/Cbc/Cbc-2.8.12.tgz'
+  sha1 '7739f1841da5b8db0ee08bc21bd1ba05bd9d8432'
+
+  depends_on 'staticfloat/juliadeps/libgfortran'
+
+  def install
+    # build without lapack until Julia issue 4923 is resolved
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "--without-lapack", 
+                          "--enable-dependency-linking"
+    system "make"
+    ENV.deparallelize  # make install fails in parallel.
+    system "make install"
+
+  end
+end
