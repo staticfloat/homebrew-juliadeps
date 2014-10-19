d8d9d5110e741c9a656338a5f39825b9437e73e5
Elliot Saba <staticfloat@gmail.com>
2014-10-18 23:28:59 -0700

    Bottle cbc

--- cbc.rb
+++ cbc.rb
@@ -9,7 +9,7 @@ class Cbc < Formula
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
     revision 1
-    sha1 '17cdf5014258b08428667d6012984efc516ca930' => :lion
+    sha1 'def6b5f597219cab01f6b71aa754abaccab4505b' => :lion
     sha1 '609fde1685f0ce02da2adaec0eaf87e75923d3f2' => :mavericks
     sha1 'de63aea362dbc498af3e8d2bbbd06d85cb937b69' => :mountain_lion
   end
