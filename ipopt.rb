8d12f1e0a413078844baa64f715b7426779318db
Elliot Saba <staticfloat@gmail.com>
2014-11-08 00:44:11 -0800

    Okay, so we need to require :fortran when building, but not when unbottling

--- ipopt.rb
+++ ipopt.rb
@@ -6,21 +6,12 @@ class Ipopt < Formula
   url 'http://www.coin-or.org/download/source/Ipopt/Ipopt-3.11.8.tgz'
   sha1 '530d718fb5a0c994c305deb3bcfdacc16cc0e2ef'
 
-  bottle do
-    root_url 'https://juliabottles.s3.amazonaws.com'
-    cellar :any
-    revision 1
-    sha1 '69e01274eb64e92572f911bc6cca9efadc82b3eb' => :lion
-    sha1 '19e1eaf45f56bda34fb81df63e96886a6f8184c8' => :mavericks
-    sha1 'c5b113de6f32c505b3836466fee044df7b382108' => :mountain_lion
-  end
-
   # Need this snippet in every formula that has a runtime dependency on libgfortran
   def post_install
     fixup_libgfortran prefix
   end
 
-  #depends_on :fortran => :build
+  depends_on :fortran => :build
   depends_on 'staticfloat/juliadeps/libgfortran'
 
   def install
