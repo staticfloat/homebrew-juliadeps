6e2cbe5e1840aaa5f02fbc59e10814897fc65157
Elliot Saba <staticfloat@gmail.com>
2014-11-15 12:23:25 -0800

    Add hwloc, prepare for bottling

---
+++ hwloc.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Hwloc < Formula
+  homepage 'http://www.open-mpi.org/projects/hwloc/'
+  url "http://www.open-mpi.org/software/hwloc/v1.9/downloads/hwloc-1.9.tar.bz2"
+  sha1 "99646446502e0f9952170bf1082be63361d99b6d"
+
+  depends_on 'staticfloat/juliadeps/pkg-config' => :build
+  depends_on 'staticfloat/juliadeps/cairo' => :optional
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
