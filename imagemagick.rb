0ff29b61676cbea73fe897ded83c33914add5036
Elliot Saba <staticfloat@gmail.com>
2014-12-01 22:49:13 -0800

    Add xz as a dependency for imagemagick

--- imagemagick.rb
+++ imagemagick.rb
@@ -28,6 +28,7 @@ class Imagemagick < Formula
 
   depends_on 'staticfloat/juliadeps/pkg-config' => :build
   depends_on 'staticfloat/juliadeps/libtool'
+  depends_on 'staticfloat/juliadeps/xz'
   depends_on 'fftw'
 
   depends_on 'jpeg' => :recommended
