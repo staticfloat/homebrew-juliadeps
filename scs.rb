196725a527a6945d9f28ac8330c52b4e10f55180
Elliot Saba <staticfloat@gmail.com>
2014-12-19 13:06:21 -0800

    Environment variables FTW

--- scs.rb
+++ scs.rb
@@ -6,9 +6,15 @@ class Scs < Formula
   head "https://github.com/cvxgrp/scs.git"
   sha1 "fac3869989770f60d451fe4bd3c14afb7f73ae9f"
 
+  bottle do
+    root_url 'https://juliabottles.s3.amazonaws.com'
+    cellar :any
+  end
+
   def install
-    default_cflags="-g -Wall -pedantic -O3 -funroll-loops -Wstrict-prototypes -I. -Iinclude"
-    system "make CFLAGS='#{default_cflags} -DDLONG' USE_LAPACK=1 LDFLAGS=''"
+    ENV['CFLAGS'] = "-DDLONG -DLAPACK_LIB_FOUND"
+    ENV['LDFLAGS'] = "-undefined suppress -flat_namespace"
+    system "make out/libscsdir.dylib"
     lib.install "out/libscsdir.dylib"
   end
 end
