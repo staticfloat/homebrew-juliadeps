77bb3af9e4bbc7994cb0492677669e3aa71f5266
Tony Kelman <tony@kelman.net>
2015-01-27 20:08:03 -0800

    Create couenne.rb and cppad.rb

---
+++ optimizationservices.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Optimizationservices < Formula
+  homepage 'https://projects.coin-or.org/OS'
+  url 'http://www.coin-or.org/download/pkgsource/OS/OS-2.9.1.tgz'
+  sha1 'fd7bd169fb3925436f7a7ebdbf676a20b79140f0'
+
+  depends_on 'staticfloat/juliadeps/pkg-config' => :build
+  depends_on 'staticfloat/juliadeps/couenne'
+  depends_on 'homebrew/science/cppad' => :build
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make -C test alltests"
+    ENV.deparallelize  # make install fails in parallel.
+    system "make install"
+  end
+end
