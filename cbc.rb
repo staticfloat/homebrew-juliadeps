61a27bc7f252fe39790c323a546c1681fff5d29d
Elliot Saba <staticfloat@gmail.com>
2015-02-10 23:52:22 -0800

    Remove bottle stanza to prepare for bottling

--- cbc.rb
+++ cbc.rb
@@ -8,11 +8,6 @@ class Cbc < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    revision 1
-    sha1 '17cdf5014258b08428667d6012984efc516ca930' => :lion
-    sha1 '9e998d8b6789faa90079e292f7b11e8501a235c5' => :mavericks
-    sha1 '0075d399760f53fe10847ea7c4c9c4f2e6b18f00' => :mountain_lion
-    sha1 "7aadbe851f313b7c7b20dcd5ff9906784b866dbb" => :yosemite
   end
 
   def install
