7f845ee77cf0a8111de0e05b926fa08a79fede1c
Elliot Saba <staticfloat@gmail.com>
2015-02-11 00:39:07 -0800

    Bottle cbc 2.9.2

--- cbc.rb
+++ cbc.rb
@@ -8,6 +8,9 @@ class Cbc < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
+    sha1 "bc732054768c20a2deb0245ebb7a4bd1dcef3e28" => :mountain_lion
+    sha1 "d7506008d1e3a5ed80cd767235f97c171866999b" => :yosemite
+    sha1 "b7ba397c0151f96057bcb6573346b6d295cd8099" => :mavericks
   end
 
   def install
