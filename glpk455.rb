01045db48f7fa2ddfc0d2aaee7eed613bbeaabe0
Elliot Saba <staticfloat@gmail.com>
2015-05-23 14:09:49 -0700

    Bump GLPK to 4.55

--- glpk455.rb
+++ glpk455.rb
@@ -9,6 +9,9 @@ class Glpk455 < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
+    sha256 "bef343addd233b4898654df05cf2fc017ee6c5d1e1ed9bb5245118431dffafd8" => :mountain_lion
+    sha256 "5e4c0b71643e88bf4949f9a6929fe975ba6635ec5979b047442590fd7c26c11d" => :yosemite
+    sha256 "1465b8cd1eb4a6fa6c5a791422d24aa8496e224962c5d2fcd469deeb99caef6a" => :mavericks
   end
 
   def install
