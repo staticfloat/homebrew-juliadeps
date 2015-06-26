5883678a2cbd6724c273927b37a696ac6ec700a4
Elliot Saba <staticfloat@gmail.com>
2015-06-25 21:55:43 -0700

    Bottle scs 1.1.5 with fixed flag

--- scs.rb
+++ scs.rb
@@ -10,6 +10,9 @@ class Scs < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
+    sha256 "eba963c11b7b36b719f92e759b32f82092838336fb2217cf19925399a1348683" => :yosemite
+    sha256 "f8c9b6148fcef9883bc100391451ad46f30d8517e5d8f218a49c98f629f5ef21" => :mavericks
+    sha256 "3c016c1a9b540eefa726907ac8e4d046518ab6e113fc1e8d9a05ee3bfc066dc8" => :mountain_lion
   end
 
   def install
