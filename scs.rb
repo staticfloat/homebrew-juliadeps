ec2db855faf54ac3a7ac534a4b90ebdb34be4737
Elliot Saba <staticfloat@gmail.com>
2015-06-27 10:37:23 -0700

    Get scs v1.1.5 ready for bottling with new flags

--- scs.rb
+++ scs.rb
@@ -5,14 +5,11 @@ class Scs < Formula
   url "https://github.com/cvxgrp/scs/archive/v1.1.5.tar.gz"
   head "https://github.com/cvxgrp/scs.git"
   sha1 "fd6c7a1a5bd428f61f9e27e0e194bff8fd43d267"
-  revision 1
+  revision 2
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha256 "eba963c11b7b36b719f92e759b32f82092838336fb2217cf19925399a1348683" => :yosemite
-    sha256 "f8c9b6148fcef9883bc100391451ad46f30d8517e5d8f218a49c98f629f5ef21" => :mavericks
-    sha256 "3c016c1a9b540eefa726907ac8e4d046518ab6e113fc1e8d9a05ee3bfc066dc8" => :mountain_lion
   end
 
   def install
