4ab6574007c0204d2f4f31f113d34a87864946a2
Elliot Saba <staticfloat@gmail.com>
2015-07-26 21:37:20 -0700

    Get ipopt ready for building

--- ipopt.rb
+++ ipopt.rb
@@ -5,14 +5,11 @@ class Ipopt < Formula
   homepage 'https://projects.coin-or.org/Ipopt'
   url 'http://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.1.tgz'
   sha1 'cbb197f6a90e0e1d64e438a5159da5f33f06aa08'
-  revision 1
+  revision 2
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha1 "f9fad5ca5e649554b7e308a4b1174c77638ad002" => :yosemite
-    sha1 "83ef53cd9c767c0a561fb26cdeaee5fbe9f33403" => :mavericks
-    sha1 "1f8ff3789dde7c20588f2ab955025c7db5c2ecab" => :mountain_lion
   end
 
   # Need this snippet in every formula that has a runtime dependency on libgfortran
