b5513b68bb8078e6525ec9fa7a4355804b405484
Godisemo <carl.akerlindh@gmail.com>
2015-09-20 12:20:25 +0200

    Bump Cairo version to 1.14.2

--- cairo.rb
+++ cairo.rb
@@ -2,16 +2,13 @@ require 'formula'
 
 class Cairo < Formula
   homepage 'http://cairographics.org/'
-  url "http://cairographics.org/releases/cairo-1.14.0.tar.xz"
-  mirror "http://www.mirrorservice.org/sites/ftp.netbsd.org/pub/pkgsrc/distfiles/cairo-1.14.0.tar.xz"
-  sha256 "2cf5f81432e77ea4359af9dcd0f4faf37d015934501391c311bfd2d19a0134b7"
-  
+  url "http://cairographics.org/releases/cairo-1.14.2.tar.xz"
+  mirror "https://www.mirrorservice.org/sites/ftp.netbsd.org/pub/pkgsrc/distfiles/cairo-1.14.2.tar.xz"
+  sha256 "c919d999ddb1bbbecd4bbe65299ca2abd2079c7e13d224577895afa7005ecceb"
+
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha1 "ccdbd258f4d9bdd9b7151b793e18290dde673c6d" => :yosemite
-    sha1 "c17c00dda78731dd4293e4c7f7fcc1f3d3384997" => :mavericks
-    sha1 "cbf83223fabea2a31896d5172020d41134e03dc1" => :mountain_lion
   end
 
   option :universal
