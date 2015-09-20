d5616cf8370dad1bd7e0f3c29c94b70baa46457a
Godisemo <carl.akerlindh@gmail.com>
2015-09-20 12:27:11 +0200

    Make all strings double quoted

--- cairo.rb
+++ cairo.rb
@@ -1,24 +1,24 @@
-require 'formula'
+require "formula"
 
 class Cairo < Formula
-  homepage 'http://cairographics.org/'
+  homepage "http://cairographics.org/"
   url "http://cairographics.org/releases/cairo-1.14.2.tar.xz"
   mirror "https://www.mirrorservice.org/sites/ftp.netbsd.org/pub/pkgsrc/distfiles/cairo-1.14.2.tar.xz"
   sha256 "c919d999ddb1bbbecd4bbe65299ca2abd2079c7e13d224577895afa7005ecceb"
 
   bottle do
-    root_url 'https://juliabottles.s3.amazonaws.com'
+    root_url "https://juliabottles.s3.amazonaws.com"
     cellar :any
   end
 
   option :universal
 
-  depends_on 'staticfloat/juliadeps/pkg-config' => :build
-  depends_on 'staticfloat/juliadeps/freetype'
-  depends_on 'staticfloat/juliadeps/fontconfig'
-  depends_on 'libpng'
-  depends_on 'staticfloat/juliadeps/pixman'
-  depends_on 'staticfloat/juliadeps/glib'
+  depends_on "staticfloat/juliadeps/pkg-config" => :build
+  depends_on "staticfloat/juliadeps/freetype"
+  depends_on "staticfloat/juliadeps/fontconfig"
+  depends_on "libpng"
+  depends_on "staticfloat/juliadeps/pixman"
+  depends_on "staticfloat/juliadeps/glib"
 
   def install
     ENV.universal_binary if build.universal?
@@ -37,6 +37,6 @@ class Cairo < Formula
     ]
 
     system "./configure", *args
-    system "make install"
+    system "make", "install"
   end
 end
