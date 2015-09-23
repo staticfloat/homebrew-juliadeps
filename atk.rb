3feb8c989e64c3d133d4cd8a93f461546008e35b
Elliot Saba <staticfloat@gmail.com>
2015-09-23 00:09:44 -0700

    Bottle atk 2.16.0

--- atk.rb
+++ atk.rb
@@ -9,6 +9,9 @@ class Atk < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
+    sha256 "3d1fac6d50e12d5f0337b9849035afd2abadb4d4438894495ae01ce2c2df1ce2" => :mavericks
+    sha256 "26381cb8d5aa0958c84ffd2803d419953369af6a57ea5d41b46ce471e6efb4f8" => :mountain_lion
+    sha256 "bff491c7eb8c6de9fff1717f2af281cf7a4e0a72d9c597d9e7c4945e3a6f6abf" => :yosemite
   end
 
   depends_on 'staticfloat/juliadeps/pkg-config' => :build
