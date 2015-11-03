cddd8d3ba9872bb1e7ba8747c7b3759c7375188d
Elliot Saba <staticfloat@gmail.com>
2015-11-03 10:41:11 -0800

    Prepare zeromq32 v3.2.5 for bottling

--- zeromq32.rb
+++ zeromq32.rb
@@ -2,16 +2,11 @@ require 'formula'
 
 class Zeromq32 < Formula
   homepage 'http://www.zeromq.org/'
-  url 'http://download.zeromq.org/zeromq-3.2.4.tar.gz'
-  sha1 '08303259f08edd1faeac2e256f5be3899377135e'
+  url 'http://download.zeromq.org/zeromq-3.2.5.tar.gz'
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha1 '108a85f8909ac01aec2a79f79d89bb22f647e0be' => :lion
-    sha1 '8a86c0e14ce29098171070b1b053390af1604fbf' => :mavericks
-    sha1 '36af54400e7e3334fae374fdc79d0eb66a985b10' => :mountain_lion
-    sha1 "7d4ca42eb9ec92140cc4c8b4603a8a9bbf8554c1" => :yosemite
   end
 
   option :universal
