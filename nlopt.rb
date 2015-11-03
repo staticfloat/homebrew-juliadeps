3c2e021ee9e7c552845a5708df11532bcedfccfa
Elliot Saba <staticfloat@gmail.com>
2015-11-03 10:40:53 -0800

    Prepare nlopt for bottling

--- nlopt.rb
+++ nlopt.rb
@@ -3,15 +3,12 @@ require 'formula'
 class Nlopt < Formula
   homepage 'https://projects.coin-or.org/Ipopt'
   url 'http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz'
-  sha1 '838c399d8fffd7aa56b20231e0d7bd3462ca0226'
+  sha256 '8099633de9d71cbc06cd435da993eb424bbcdbded8f803cdaa9fb8c6e09c8e89'
+  revision 1
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha1 '191ae665e492b100443c23b6e051001b3ef037b9' => :lion
-    sha1 '920fe73beebeb82220539e9bf15349fb0a149f0e' => :mavericks
-    sha1 'b58d6ff4b118bc894d7138f53d0b52ee673463aa' => :mountain_lion
-    sha1 "b315326ff1ac4053a7ecc0afe3462b0751e0c5d1" => :yosemite
   end
 
   def install
