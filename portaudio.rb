e5537176db198a0f36c4a6c7ea0783d39c18ecad
Elliot Saba <staticfloat@gmail.com>
2015-11-04 11:32:13 -0800

    Cleanup portaudio a bit

--- portaudio.rb
+++ portaudio.rb
@@ -5,14 +5,7 @@ class Portaudio < Formula
 
   stable do
     url 'http://www.portaudio.com/archives/pa_stable_v19_20111121.tgz'
-    sha1 'f07716c470603729a55b70f5af68f4a6807097eb'
-
-    # Fix PyAudio compilation on Lion
-    patch :p0 do
-      url "https://trac.macports.org/export/94150/trunk/dports/audio/portaudio/files/patch-include__pa_mac_core.h.diff"
-      mirror "https://raw.githubusercontent.com/sakuro/macports/9ee0b7dd6045a3967348638a228bc1a658bcaa3f/audio/portaudio/files/patch-include__pa_mac_core.h.diff"
-      sha1 "28188dfb6a5438a050fccb70331751a40e33447d"
-    end if MacOS.version >= :lion
+    sha256 '9c26d1330d506496789edafe55b0386f20d83c4aa2c0e3f81fbeb0f114ab1b99'
   end
 
   head 'https://subversion.assembla.com/svn/portaudio/portaudio/trunk/', :using => :svn
@@ -20,9 +13,7 @@ class Portaudio < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha1 '7525cd47924d4adb45be600c936d2abc77c2fa46' => :lion
     sha1 '316130397237ff3fb01cd866f0e6e151d63e0f5d' => :mavericks
-    sha1 'bdeb69787a9983f61b8239cba74face75a1dec37' => :mountain_lion
     sha1 "876acc12c9699e12399c96c6bc3af57c200e97d1" => :yosemite
   end
 
