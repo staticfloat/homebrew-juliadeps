fcb72e8f1cac5fa47abd334b4e7b8ff073710953
Elliot Saba <staticfloat@gmail.com>
2015-11-04 09:19:27 -0800

    Prepare hwloc v1.11.1 for bottling

--- hwloc.rb
+++ hwloc.rb
@@ -2,15 +2,12 @@ require 'formula'
 
 class Hwloc < Formula
   homepage 'http://www.open-mpi.org/projects/hwloc/'
-  url "http://www.open-mpi.org/software/hwloc/v1.9/downloads/hwloc-1.9.tar.bz2"
-  sha1 "99646446502e0f9952170bf1082be63361d99b6d"
+  url 'http://www.open-mpi.org/software/hwloc/v1.11/downloads/hwloc-1.11.1.tar.bz2'
+  sha256 '48614061c4baa44dec7ec078459c3bd1f3ecaa39a39331a7eb857c945cca6c0f'
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha1 "560982226130908dfb6e715c02c28459e7b81c60" => :mountain_lion
-    sha1 "7e9351ef9e1553bdf2931a6dc972e4c1e7327679" => :mavericks
-    sha1 "737942aee6397af2f566c0a4f3941f242f84bd25" => :yosemite
   end
 
   depends_on 'staticfloat/juliadeps/pkg-config' => :build
