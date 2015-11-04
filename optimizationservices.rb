f8b78729661fc31fbbc0a6ca6ffde09e8991ba1c
Elliot Saba <staticfloat@gmail.com>
2015-11-04 15:41:54 -0800

    Prepare optimizationservices v2.10.1 for bottling

--- optimizationservices.rb
+++ optimizationservices.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Optimizationservices < Formula
   homepage 'https://projects.coin-or.org/OS'
-  url 'http://www.coin-or.org/download/pkgsource/OS/OS-2.9.2.tgz'
-  sha1 '90340631a5ce7fa05a67484d624d1692473e9763'
+  url 'http://www.coin-or.org/download/pkgsource/OS/OS-2.10.1.tgz'
+  sha256 'fb171e705b81be59f7ede188f29aa0720c558e1e00da74920dce894e7f9e72eb'
 
   depends_on 'staticfloat/juliadeps/pkg-config' => :build
   depends_on 'staticfloat/juliadeps/couenne'
@@ -12,8 +12,6 @@ class Optimizationservices < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha256 "939e7d3a5d990cb5b14d840f414f7b1b76155474bd9e5b6079c311b6590e2f29" => :mavericks
-    sha256 "9349c42d184cb80bcb1566d2def3e325d2b6a7b85af8eeb8430cf60719e21654" => :yosemite
   end
 
   def install
