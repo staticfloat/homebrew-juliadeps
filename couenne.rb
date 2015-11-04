d67326123e5287fb5782997f0290f12322cc50d3
Elliot Saba <staticfloat@gmail.com>
2015-11-04 15:24:04 -0800

    Prepare Couenne v0.5.6 for bottling

--- couenne.rb
+++ couenne.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Couenne < Formula
   homepage 'https://projects.coin-or.org/Couenne'
-  url 'http://www.coin-or.org/download/pkgsource/Couenne/Couenne-0.5.4.tgz'
-  sha1 '1081ba62447744b27c5bc67d3d2103a3bcc42ed9'
+  url 'http://www.coin-or.org/download/pkgsource/Couenne/Couenne-0.5.6.tgz'
+  sha256 '60b5218ff1d878868a18e96f74cb3e816743f68b327522641c5ea8f1174ee7a6'
 
   depends_on 'staticfloat/juliadeps/pkg-config' => :build
   depends_on 'staticfloat/juliadeps/bonmin'
@@ -11,8 +11,6 @@ class Couenne < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha256 "5b50a589ada479b95466e7c9144c22f4ae3aaf156592c335c0882b392284e5ee" => :yosemite
-    sha256 "6ec4d4ff3720a09478f760e0820037922d6d6e952d894eb465d51c821cc620fe" => :mavericks
   end
 
   def install
