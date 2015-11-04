563d82e97e739eeb20d4acd90602e3d1e620ff97
Elliot Saba <staticfloat@gmail.com>
2015-11-04 10:22:20 -0800

    Bottle hwloc v1.11.1

--- hwloc.rb
+++ hwloc.rb
@@ -8,6 +8,9 @@ class Hwloc < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
+    sha256 "54c6c707dee5769903d9238da24167e8be3debaceb0f1f075d3774f7e7fc0f8d" => :mavericks
+    sha256 "28d769052327a2dd9f68b950825e2c66b47fc5c02b6e7e940be867091e499f72" => :yosemite
+    sha256 "3357af9fe518f301e2feca9ee5214904ec49152808ab8854d21701cf3a91f044" => :el_capitan
   end
 
   depends_on 'staticfloat/juliadeps/pkg-config' => :build
