efd2980b17cce739df76594e33b9346109ea0e57
Elliot Saba <staticfloat@gmail.com>
2015-11-04 11:36:58 -0800

    We need a revision here too

--- szip.rb
+++ szip.rb
@@ -4,6 +4,7 @@ class Szip < Formula
   homepage 'http://www.hdfgroup.org/HDF5/release/obtain5.html#extlibs'
   url 'http://www.hdfgroup.org/ftp/lib-external/szip/2.1/src/szip-2.1.tar.gz'
   sha256 'a816d95d5662e8279625abdbea7d0e62157d7d1f028020b1075500bf483ed5ef'
+  revision 1
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
