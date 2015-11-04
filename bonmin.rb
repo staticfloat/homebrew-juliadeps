e015b205c0d166e9a56fb7b99ddd39bb8c7d2647
Elliot Saba <staticfloat@gmail.com>
2015-11-04 15:22:49 -0800

    Bottle bonmin 1.8.4

--- bonmin.rb
+++ bonmin.rb
@@ -12,6 +12,9 @@ class Bonmin < Formula
   bottle do
     cellar :any
     root_url 'https://juliabottles.s3.amazonaws.com'
+    sha256 "7ab7608e41289cfeac476adf07badb84fbe0711e7a44015e9c826d155b3f54f2" => :mavericks
+    sha256 "d7591823a58f4c672ffbb3ea8aad910e81a37283e19025da42c1e0702814e38a" => :yosemite
+    sha256 "d8b1db5e5b921f0e518257e6b77c999dc67b11e172570c4c53f3eaab128bf2ed" => :el_capitan
   end
 
   def install
