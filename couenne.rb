773c9380ff72d1b0cb96877c7f40f3485672e472
Elliot Saba <staticfloat@gmail.com>
2015-11-04 15:37:12 -0800

    Bottle Couenne v0.5.6

--- couenne.rb
+++ couenne.rb
@@ -11,6 +11,9 @@ class Couenne < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
+    sha256 "5945884f8192d5705f6011db410a7466c2c65295d02d02b21b049d707df1fcdf" => :yosemite
+    sha256 "519ff4a8f8fd2a5cfb5cd8b7a6e4306f09c728195cb1c76b5156d4270b35b31d" => :el_capitan
+    sha256 "d47d6572c2a7b2157c014e2c0b2313b7f77a030a41d14b90325433b9b00362cc" => :mavericks
   end
 
   def install
