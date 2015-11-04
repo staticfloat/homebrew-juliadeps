059bb040de51fbd9400462c8c640ff6a56e8a120
Elliot Saba <staticfloat@gmail.com>
2015-11-04 10:45:42 -0800

    Prepare scs v1.1.8 for bottling

--- scs.rb
+++ scs.rb
@@ -1,18 +1,14 @@
 require "formula"
 
 class Scs < Formula
-  homepage "https://github.com/cvxgrp/scs"
-  url "https://github.com/cvxgrp/scs/archive/v1.1.5.tar.gz"
-  head "https://github.com/cvxgrp/scs.git"
-  sha1 "fd6c7a1a5bd428f61f9e27e0e194bff8fd43d267"
-  revision 2
+  homepage 'https://github.com/cvxgrp/scs'
+  url 'https://github.com/cvxgrp/scs/archive/v1.1.8.tar.gz'
+  head 'https://github.com/cvxgrp/scs.git'
+  sha256 '4249df60aa3c27d6b4d46a162a766d3e391f7771ef3a3a68edd5380176c3ae92'
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha256 "e14907aa969fe3ff39d536754df0689e8359532a04085b89c7173ede5aadeec3" => :mountain_lion
-    sha256 "8e4dbc87cbc1baa283327885692333586e76cf71cad12751629f46f7334a6cb7" => :yosemite
-    sha256 "9e7aebb57823a0133faeb927f59aca7231595e2b8eac77cd185f43d7388128bc" => :mavericks
   end
 
   def install
