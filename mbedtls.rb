7d7fd4b7bf59c55f850fa2bd27d44e8a17b96398
Elliot Saba <staticfloat@gmail.com>
2015-11-04 11:47:23 -0800

    Prepare mbedtls v2.1.2 for bottling

--- mbedtls.rb
+++ mbedtls.rb
@@ -2,20 +2,17 @@
 # Forked to support bottled shared libraries, which are not built upstream
 
 class Mbedtls < Formula
-  desc "Cryptographic & SSL/TLS library"
-  homepage "https://tls.mbed.org/"
-  url "https://tls.mbed.org/download/mbedtls-2.1.1-apache.tgz"
-  sha256 "8f25b6f156ae5081e91bcc58b02455926d9324035fe5f7028a6bb5bc0139a757"
-  head "https://github.com/ARMmbed/mbedtls.git"
+  desc 'Cryptographic & SSL/TLS library'
+  homepage 'https://tls.mbed.org/'
+  url 'https://tls.mbed.org/download/mbedtls-2.1.2-apache.tgz'
+  head 'https://github.com/ARMmbed/mbedtls.git'
+  sha256 'ce57cb9085f976ffde945af7e8cec058a66ad181a96fd228fbcbc485213a7c58'
 
   depends_on "cmake" => :build
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha256 "b88d0e596a904925c4065f94421515cfd41124f6915fbdb66af4a4a7865b4e66" => :mountain_lion
-    sha256 "255a3704b37a7a151b9666a331e3be470193fea97ea629c3e85776b751046216" => :mavericks
-    sha256 "631606cd2edc6b3aa1133f346367081a87cf0472e7f54ed1c983f083bebc686c" => :yosemite
   end
 
   def install
