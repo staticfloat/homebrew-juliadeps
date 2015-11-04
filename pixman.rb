b777acf7dc247d4633a72edc16cfa8021c1dc561
Elliot Saba <staticfloat@gmail.com>
2015-11-04 11:38:19 -0800

    Prepare pixman v0.32.8 for bottling

--- pixman.rb
+++ pixman.rb
@@ -1,17 +1,13 @@
 require "formula"
 
 class Pixman < Formula
-  homepage "http://cairographics.org/"
-  url "http://cairographics.org/releases/pixman-0.32.6.tar.gz"
-  sha256 "3dfed13b8060eadabf0a4945c7045b7793cc7e3e910e748a8bb0f0dc3e794904"
+  homepage 'http://cairographics.org/'
+  url 'http://cairographics.org/releases/pixman-0.32.8.tar.gz'
+  sha256 '575ade17c40b47d391b02b4c0c63531c897b31e70046c96749514b7d8800d65d'
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    revision 1
-    sha1 "8f47ec83a3ce8bf6bf41676b3143286c8dbb85bd" => :yosemite
-    sha1 "9670f838299329540839e49026849db0f394f261" => :mavericks
-    sha1 "9012f57f5b65b3f1a3dfec6b91be7f66e955a3e4" => :mountain_lion
   end
 
   depends_on "staticfloat/juliadeps/pkg-config" => :build
