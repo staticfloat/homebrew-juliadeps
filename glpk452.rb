86dae4c8067c5eb0816c590437a1870e1a7951eb
Elliot Saba <staticfloat@gmail.com>
2015-11-09 13:58:20 -0800

    Bottle glpk v4.52 on 10.11

--- glpk452.rb
+++ glpk452.rb
@@ -4,12 +4,14 @@ class Glpk452 < Formula
   homepage 'http://www.gnu.org/software/glpk/'
   url 'http://ftpmirror.gnu.org/glpk/glpk-4.52.tar.gz'
   mirror 'http://ftp.gnu.org/gnu/glpk/glpk-4.52.tar.gz'
+  sha256 '9a5dab356268b4f177c33e00ddf8164496dc2434e83bd1114147024df983a3bb'
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
     sha1 '4e5035b64862a8ecf47637de9e7e2c0135e8d6e4' => :mavericks
     sha1 "1ccd6325aafa76657f23366b6c8e0821f16a5ec0" => :yosemite
+    sha256 "07cbe9cbdb3736fe6c558a3a1177de2a360a08527a27d88ab8a050599d119c1a" => :el_capitan
   end
 
   def install
