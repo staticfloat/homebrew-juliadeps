67dcc6015b57329e56f1f37961a1d7bb3b47ce2f
Elliot Saba <staticfloat@gmail.com>
2015-11-12 14:48:20 -0800

    Prepare cairo for bottling

--- cairo.rb
+++ cairo.rb
@@ -2,16 +2,12 @@ require "formula"
 
 class Cairo < Formula
   homepage "http://cairographics.org/"
-  url "http://cairographics.org/releases/cairo-1.14.2.tar.xz"
-  mirror "https://www.mirrorservice.org/sites/ftp.netbsd.org/pub/pkgsrc/distfiles/cairo-1.14.2.tar.xz"
-  sha256 "c919d999ddb1bbbecd4bbe65299ca2abd2079c7e13d224577895afa7005ecceb"
+  url "http://cairographics.org/releases/cairo-1.14.4.tar.xz"
+  mirror "https://www.mirrorservice.org/sites/ftp.netbsd.org/pub/pkgsrc/distfiles/cairo-1.14.4.tar.xz"
 
   bottle do
     root_url "https://juliabottles.s3.amazonaws.com"
     cellar :any
-    sha256 "a7dfb100d89da3219da13d8c8609ef8f1137fb5a0a24e46bf06748946ac4af1e" => :mountain_lion
-    sha256 "84e8622a94d0f3c3c2dfef9299e9e04526bc677c6a0acc6d1d4d48c723744d18" => :mavericks
-    sha256 "1fdd86a0ab7556a5580bd4ca4a5a6e9fe5b3d2fa62ab3169d5b90657fc581c56" => :yosemite
   end
 
   option :universal
