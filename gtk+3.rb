bbc38a984bd3dcbb3684a42581f4ac793fde6112
Elliot Saba <staticfloat@gmail.com>
2015-11-12 21:07:00 -0800

    Bottle gtk+3 v3.19.1

--- gtk+3.rb
+++ gtk+3.rb
@@ -2,11 +2,15 @@ require 'formula'
 
 class Gtkx3 < Formula
   homepage 'http://gtk.org/'
-  url "https://download.gnome.org/sources/gtk+/3.19/gtk+-3.19.1.tar.xz"
+  url 'https://download.gnome.org/sources/gtk+/3.19/gtk+-3.19.1.tar.xz'
+  sha256 'cd87b47a6f6436f16f15ebc1f0cbb113c92c603f572f920235405faaadc7bed9'
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
+    sha256 "b3f3d39d585116ecc000ad153afa32bf6fee9b10540faf2736d3629c48dcd172" => :mavericks
+    sha256 "3973f23b9db563c3ddb2da85d9bb6fde7b14d2f566a6701daa8c63faaf0ad9ae" => :yosemite
+    sha256 "a62747dd7cb31b39c45feb8490d53734a9b18ad800780ab7954fdbc98538a0b8" => :el_capitan
   end
 
   depends_on 'staticfloat/juliadeps/pkg-config' => :build
