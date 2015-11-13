615fb1e1abd1a9d66555f61d1ecaa28f880d9198
Elliot Saba <staticfloat@gmail.com>
2015-11-12 19:57:47 -0800

    New gdk-pixbuf magic

--- gdk-pixbuf.rb
+++ gdk-pixbuf.rb
@@ -4,13 +4,11 @@ class GdkPixbuf < Formula
   homepage 'http://gtk.org'
   url 'http://ftp.acc.umu.se/pub/GNOME/sources/gdk-pixbuf/2.33/gdk-pixbuf-2.33.1.tar.xz'
   sha256 '132358b39bd8204e2ef0571c14206dfdca1f44bcc94c21751d6e6b0433b5088b'
+  revision 1
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha256 "7be0256775cf75197a3e0102648a79afa646631cb5d741f932d9e32121fca54a" => :mavericks
-    sha256 "4d261ad9c1542c477711aa295af65590139729aee2d9b41a8a4da5b167541959" => :yosemite
-    sha256 "0a4fd711958a1c8a4f2e7c5e0a36c7bec3f6e114f924e983e5086f75029b148c" => :el_capitan
   end
 
   option :universal
@@ -48,15 +46,15 @@ class GdkPixbuf < Formula
 
   def post_install
     # Change the version directory below with any future update
-    ENV["GDK_PIXBUF_MODULEDIR"]="#{HOMEBREW_PREFIX}/lib/gdk-pixbuf-2.0/2.10.0/loaders"
-    ENV["GDK_PIXBUF_MODULE_FILE"]="#{lib}/gdk-pixbuf-2.0/2.10.0/loaders.cache"
+    ENV["GDK_PIXBUF_MODULEDIR"]="#{HOMEBREW_PREFIX}/lib/gdk-pixbuf-2.0/#{version}/loaders"
+    ENV["GDK_PIXBUF_MODULE_FILE"]="#{lib}/gdk-pixbuf-2.0/#{version}/loaders.cache"
     system "#{bin}/gdk-pixbuf-query-loaders", "--update-cache"
   end
 
   def caveats; <<-EOS.undent
     Programs that require this module need to set the environment variables
-      export GDK_PIXBUF_MODULEDIR="#{HOMEBREW_PREFIX}/lib/gdk-pixbuf-2.0/2.10.0/loaders"
-      export GDK_PIXBUF_MODULE_FILE="#{HOMEBREW_PREFIX}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
+      export GDK_PIXBUF_MODULEDIR="#{HOMEBREW_PREFIX}/lib/gdk-pixbuf-2.0/#{version}/loaders"
+      export GDK_PIXBUF_MODULE_FILE="#{HOMEBREW_PREFIX}/lib/gdk-pixbuf-2.0/#{version}/loaders.cache"
     If you need to manually update the query loader cache
       #{bin}/gdk-pixbuf-query-loaders --update-cache
     EOS
