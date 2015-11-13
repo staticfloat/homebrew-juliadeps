f3675f172a8eea0c47e5f9752cdaa92be430da13
Elliot Saba <staticfloat@gmail.com>
2015-11-12 17:57:38 -0800

    Prepare harfbuzz 0.9.42 for bottling

--- harfbuzz.rb
+++ harfbuzz.rb
@@ -2,15 +2,11 @@ require "formula"
 
 class Harfbuzz < Formula
   homepage "http://www.freedesktop.org/wiki/Software/HarfBuzz"
-  url "http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.37.tar.bz2"
-  sha256 "255f3b3842dead16863d1d0c216643d97b80bfa087aaa8fc5926da24ac120207"
+  url "http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.42.tar.bz2"
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha1 "6a768391ea765c78d4a6d39ccad701f68f067b65" => :yosemite
-    sha1 "fb56592f5d2cd5f70d6d0dacb3340e193c76f7c3" => :mavericks
-    sha1 "c4c61e3e044ea550f089f3e3a1bafc19c39d8bd4" => :mountain_lion
   end
 
   depends_on "staticfloat/juliadeps/pkg-config" => :build
