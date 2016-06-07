e4406321036aad4610ebc5c548e4cad17ca87ad0
Elliot Saba <staticfloat@gmail.com>
2016-06-07 16:44:06 -0700

    Update libgfortran for gcc 6.1

--- libgfortran.rb
+++ libgfortran.rb
@@ -13,6 +13,9 @@ class Libgfortran < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
+    sha256 "51a3ddd770fa725d75270eba725a5d97f374e80df98a8381c85fff44087348e8" => :el_capitan
+    sha256 "04844f55ee5324bfbabaa8d2a75d53765c3686afb57f0ddfbf56e55f5a3bd399" => :mavericks
+    sha256 "c34a4b0b7b0ba2ab086c6e41066d2ca24a45f54841b026122373dbc6446a20b3" => :yosemite
   end
 
   depends_on 'gcc' => :build
