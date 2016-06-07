f3941a427ea532aa6df0a145c9136b5208ada1af
Elliot Saba <staticfloat@gmail.com>
2016-06-07 16:28:22 -0700

    Bump libgfortran to 6.1 and prepare for bottling

--- libgfortran.rb
+++ libgfortran.rb
@@ -7,16 +7,12 @@ require "#{File.dirname(__FILE__)}/libgfortran"
 class Libgfortran < Formula
   homepage 'http://gcc.gnu.org/wiki/GFortran'
   url 'https://github.com/staticfloat/homebrew-libgfortran-formula/archive/master.tar.gz'
-  sha1 '09328c065c42051fab341e660837704a3b1f5d4a'
-  version '5.2'
+  sha256 'daa3d315c20d39504e94318254968d97f9de6096900de00ea0dc40bb541154b4'
+  version '6.1'
 
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
-    sha256 "655df1ac79b0e63346ed97f333cc667ad6760d490f932412735e2bd516cc1563" => :mountain_lion
-    sha256 "4322096e295687709c992904248bace14d4b89d0774c7f97bb402b3d8a10ef60" => :mavericks
-    sha256 "0a54c14b84c33d4249df4ab2f139c81d6d5a85fd2e449ab1c97ee759382397c4" => :yosemite
-    sha256 "ce6336a6e26df4764b220a7ffaca82b7fcae2d9dd305167f80dd59201e775401" => :el_capitan
   end
 
   depends_on 'gcc' => :build
