a2e2f462fb9f0afe5972bc3980b88777839f3123
Elliot Saba <staticfloat@gmail.com>
2016-06-09 12:47:18 -0700

    We need to keep `ipopt.rb` and `libgfortran.rb` around for sure

---
+++ ipopt.rb
@@ -0,0 +1,42 @@
+require 'formula'
+require "#{File.dirname(__FILE__)}/libgfortran"
+
+class Ipopt < Formula
+  homepage 'https://projects.coin-or.org/Ipopt'
+  url 'http://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.4.tgz'
+  sha256 '292afd952c25ec9fe6225041683dcbd3cb76e15a128764671927dbaf881c2e89'
+
+  bottle do
+    root_url 'https://juliabottles.s3.amazonaws.com'
+    cellar :any
+    sha256 "b8f2f83776830d969841d96a3f35d62015b747f6d95378f17d9c6ba90a4ff70a" => :mavericks
+    sha256 "83926ec5293dc95ce0bce1a4e8c8cc6d5c4a5ae2056c790e3172663b1fcde851" => :yosemite
+    sha256 "14ad4d4689b3dbb061c6d68b9a57113f7a27b823d297d54c56c3f61d8208087c" => :el_capitan
+  end
+
+  # Need this snippet in every formula that has a runtime dependency on libgfortran
+  def post_install
+    fixup_libgfortran prefix
+  end
+
+  # Need to enable this when building the bottle, disable it when installing from bottles
+  depends_on :fortran if ARGV.build_bottle?
+  depends_on 'staticfloat/juliadeps/libgfortran'
+
+  def install
+    system "cd ThirdParty/Blas; ./get.Blas"
+    system "cd ThirdParty/Blas; ./configure --prefix=#{prefix} --disable-shared --with-pic; make install"
+    system "cd ThirdParty/Lapack; ./get.Lapack"
+    system "cd ThirdParty/Lapack; ./configure --prefix=#{prefix} --disable-shared --with-pic; make install"
+    system "cd ThirdParty/ASL; ./get.ASL"
+    system "cd ThirdParty/Mumps; ./get.Mumps"
+    system "./configure", "--disable-dependency-tracking",
+                          "--with-blas=#{prefix}/lib/libcoinblas.a",
+                          "--with-lapack=#{prefix}/lib/libcoinlapack.a",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make test"
+    ENV.deparallelize  # make install fails in parallel.
+    system "make install"
+  end
+end
