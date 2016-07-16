d30a2103a16c5daf26af63aecb2bbec165d9a8a1
Alex Arslan <ararslan@comcast.net>
2016-07-15 17:57:10 -0700

    Remove my own bottle

--- rmath-julia.rb
+++ rmath-julia.rb
@@ -6,13 +6,6 @@ class RmathJulia < Formula
   sha256 "8ba59378273dea999f956cecf37d47723682f3a46b55ce76ec36a0ffdbe6c8f7"
   head "https://github.com/JuliaLang/Rmath-julia.git", :branch => "master"
 
-  bottle do
-    root_url "http://dl.bintray.com/ararslan/Rmath-julia/"
-    cellar :any
-    revision 1
-    sha256 "b2a96543194c4e610035760e87733e6dc7383686f8c5e88eb6775644010a3086" => :yosemite
-  end
-
   option "with-clang", "Compile using Clang instead of GCC"
 
   depends_on :gcc => :build unless "with-clang"
