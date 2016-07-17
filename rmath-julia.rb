31d83665a55532083b60d22171f4887959bcac25
Elliot Saba <staticfloat@gmail.com>
2016-07-16 22:28:05 -0700

    Bottle rmath-julia v0.1

--- rmath-julia.rb
+++ rmath-julia.rb
@@ -10,6 +10,14 @@ class RmathJulia < Formula
 
   depends_on :gcc => :build unless "with-clang"
 
+  bottle do
+    root_url 'https://juliabottles.s3.amazonaws.com'
+    cellar :any
+    sha256 "d02827639cdd69a3eebf76665bbe1c812a6ee6e20d50e3df5046eafe221f307b" => :mavericks
+    sha256 "32d8595c2c672c8065de7e6d3e304daaef1967333d101e39fcf3ed9cd78d6a92" => :yosemite
+    sha256 "7ecafc948e6bb94335e0a626b86e5ab588435af4420c16f40ff0b326e6f05cb4" => :el_capitan
+  end
+
   def install
     args = []
     args << "USECLANG=1" if build.with? "clang"
