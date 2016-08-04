1dcb2142b6887bf90df2d32858e10f2f26c40d57
Elliot Saba <staticfloat@gmail.com>
2016-08-04 09:20:57 -0700

    Bottle ECOS v2.0.5

--- ecos.rb
+++ ecos.rb
@@ -9,6 +9,9 @@ class Ecos < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
+    sha256 "d72f245ec2decbd4803f484e8beac4a6dfd02d97d4001e15ea1c65232fe76ddb" => :mavericks
+    sha256 "2c78a18c209fe1f78176f1250386f39313dbc63000be224376acc25ee828defc" => :el_capitan
+    sha256 "9984135adb3e015a5294a9e9b769e26ff718f0a3b9c98a2870b7b03c1247958b" => :yosemite
  end
 
   def install
