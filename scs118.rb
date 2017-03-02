1bef7057db778e559f1c533fa67f10f7fc8f44b8
Elliot Saba <staticfloat@gmail.com>
2017-03-02 00:14:32 -0800

    Bottle SCS `v1.1.8` for Julia v0.4 posterity

--- scs118.rb
+++ scs118.rb
@@ -9,6 +9,10 @@ class Scs118 < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
+    sha256 "e1de987f1a1e0a3dadd1b8e160fd0b8ba67ea5630c83a97cc4fd71eacd03cd3d" => :mavericks
+    sha256 "c895e43c4619b8f0ff8cfdddd79bf41bb9dae54ed81487591211bd1b4b13fe71" => :yosemite
+    sha256 "ea4f3264c5fff0b8e65419119c0a1f40d9f055777d3aa120b677f32d7ba852d8" => :sierra
+    sha256 "862356fc4324b5d22be02449e9049af92115349e9801ff82969d04d28363f7ad" => :el_capitan
   end
 
   def install
