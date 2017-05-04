a395ca67156b094d774a2cf3b990f08207ad2519
Elliot Saba <staticfloat@gmail.com>
2017-05-04 13:38:06 -0700

    Bottle glpk461

--- glpk461.rb
+++ glpk461.rb
@@ -9,6 +9,10 @@ class Glpk461 < Formula
   bottle do
     root_url 'https://juliabottles.s3.amazonaws.com'
     cellar :any
+    sha256 "2a90938fda7150ad68b522130c9e77637922c0c4fc1fe2f9ce31161214a3a09f" => :mavericks
+    sha256 "5f0d006468727f1e8cf244de36c0dd8656b31e0fd7fb3981c0d5443f8c5acb92" => :yosemite
+    sha256 "5394c6d5d80c170fcb8124161801316a0534c43ac78842e64d18b9a89eb88a24" => :el_capitan
+    sha256 "ff2e684a24b5858e3032e384f76e947b902550ad8ee5a0fbc3c211c20064a611" => :sierra
   end
 
   def install
